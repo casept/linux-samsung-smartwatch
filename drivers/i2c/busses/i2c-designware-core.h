2e215af2f5f98c2b9dce241275869ce2d47b5a02
Wolfram Sang <wsa+renesas@sang-engineering.com>
2024-01-14 08:40:31 +0100

    Revert "i2c: designware: Consolidate PM ops"
    This reverts commit 2347b8dc0d2e53a980ea2d74af2a77b2f9ccfed2.
    Regressions have been reported, the series needs more work.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- drivers/i2c/busses/i2c-designware-core.h
+++ drivers/i2c/busses/i2c-designware-core.h
@@ -15,7 +15,6 @@
 #include <linux/dev_printk.h>
 #include <linux/errno.h>
 #include <linux/i2c.h>
-#include <linux/pm.h>
 #include <linux/regmap.h>
 #include <linux/types.h>
 
@@ -338,8 +337,7 @@ int i2c_dw_wait_bus_not_busy(struct dw_i2c_dev *dev);
 int i2c_dw_handle_tx_abort(struct dw_i2c_dev *dev);
 int i2c_dw_set_fifo_size(struct dw_i2c_dev *dev);
 u32 i2c_dw_func(struct i2c_adapter *adap);
-
-extern const struct dev_pm_ops i2c_dw_dev_pm_ops;
+void i2c_dw_disable(struct dw_i2c_dev *dev);
 
 static inline void __i2c_dw_enable(struct dw_i2c_dev *dev)
 {
@@ -354,7 +352,6 @@ static inline void __i2c_dw_disable_nowait(struct dw_i2c_dev *dev)
 }
 
 void __i2c_dw_disable(struct dw_i2c_dev *dev);
-void i2c_dw_disable(struct dw_i2c_dev *dev);
 
 extern void i2c_dw_configure_master(struct dw_i2c_dev *dev);
 extern int i2c_dw_probe_master(struct dw_i2c_dev *dev);
